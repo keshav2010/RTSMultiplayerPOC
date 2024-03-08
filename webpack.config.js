80d43c7f1725e7acb607eaa8ef07f89f00ca2a25
keshav2010 <sharmakeshav15157@gmail.com>
2024-03-09 01:43:09 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js', '.tsx'],
     },
     //loader for css
     module: {
