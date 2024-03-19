bd8fdfaac02ddba89bc66510146860a44b64fcf4
keshav2010 <sharmakeshav15157@gmail.com>
2024-03-19 23:40:56 +0530

    create soldier based on game rules

--- gameserver/commands/OnSoldierCreateCommand.ts
+++ gameserver/commands/OnSoldierCreateCommand.ts
@@ -1,20 +1,12 @@
 // OnJoinCommand.ts
 import { Command } from "@colyseus/command";
 import { SessionRoom } from "../SessionRoom";
-import { Client } from "colyseus";
-import { GameStateManager } from "../core/GameStateManager";
 import { CommandPayload } from "./CommandPayloadType";
-import { SoldierState } from "../schema/SoldierState";
 export class OnSoldierCreateCommand extends Command<
   SessionRoom,
   CommandPayload
 > {
   execute({ client, message, gameManager }: CommandPayload) {
-    const soldierId = this.state.getPlayer(client.id)?.addNewSoldier(message.soldierType, gameManager!.scene);
-    if(!soldierId) {
-      return;
-    }
-    return;
     console.log('received soldier create request, queueing');
     let lengthBefore = this.state.getPlayer(client.id)?.spawnRequestDetailMap.size;
     this.state.getPlayer(client.id)?.queueSpawnRequest(message.soldierType);
