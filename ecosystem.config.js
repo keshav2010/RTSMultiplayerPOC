99bc4f51e2d4f319a4623ce963d0ad8ba28e9d23
keshav sharma <sharmakeshav15157@gmail.com>
2025-07-27 23:13:44 +0530

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 // ecosystem.config.js : This file is used to manage multiple applications that can be managed via pm2
 const os = require('os');
 require('dotenv').config();
+
+console.log('[ecosystem.config.js] CPU Count ', os.cpus().length);
+
 module.exports = {
     apps: [{
         port        : process.env.PORT || 3000,
@@ -8,7 +11,7 @@ module.exports = {
         script      : "gameserver_dist/gameserver/index.js", // your entrypoint file
         time        : true,
         watch       : process.env.NODE_ENV !== 'production',           // optional
-        instances   : Math.max(os.cpus().length-1, 1),
+        instances   : process.env.FIX_INSTANCEMath.max(os.cpus().length-1, 1),
         wait_ready  : process.env.NODE_ENV === 'production',
         exec_mode   : 'fork',         // IMPORTANT: do not use cluster mode.
         env: {
